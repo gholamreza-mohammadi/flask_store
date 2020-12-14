e11faadcec61de9efcee7cf00de72905fd4247a4
gholamrezaMohammadi <gholamreza13720224@gmail.com>
2020-12-14 19:08:17 +0330

    code cleaning

--- flask_store/__init__.py
+++ flask_store/__init__.py
@@ -37,9 +37,9 @@ def create_app(test_config=None):
     #
     # # apply the blueprints to the app
     from flask_store import admin
-    
+
     app.register_blueprint(admin.bp)
-    
+
     # # make url_for('index') == url_for('blog.index')
     # # in another app, you might define a separate main index here with
     # # app.route, while giving the blog blueprint a url_prefix, but for
