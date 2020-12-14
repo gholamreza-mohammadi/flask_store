e560d526bf31414d2d0c749a1616eabd1a7eda33
Reza Ghanbari <rezaghanbarii2000@gmail.com>
2020-12-14 23:04:12 +0330

    add cryptography

--- flask_store/admin.py
+++ flask_store/admin.py
@@ -1,6 +1,7 @@
 from flask import Blueprint ,request,render_template , url_for,current_app
 import json
 from flask import redirect
+from cryptography.fernet import Fernet
 
 bp = Blueprint("admin", __name__, url_prefix="/admin")
 
@@ -11,8 +12,10 @@ def login():
         password = request.form["password"] 
         with open("flask_store/users.json") as user_file:
             users = json.load(user_file)
+            cipher_suite = Fernet(current_app.config['CIPHER_KEY'])
             for user in users:
-                if username == user[0] and password == user[1]:
+                unciphered_pass = (cipher_suite.decrypt(bytes(user[1],'utf-8'))).decode("utf-8")
+                if username == user[0] and password == unciphered_pass:
                     return redirect(url_for("hello"))
             return render_template("login.html",error="incorect user or pass")
     else:
--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,3 @@
 SECRET_KEY = b'78118c962e43ee0aac7c5ca84b4604ad81c122f8cced07eb'
+CIPHER_KEY = b'mjgLX1X4Fv-eQwga6MPz3LFquxgTDgm895bdIiVbwaQ='
 DATABASE = ''
