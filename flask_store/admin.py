28f2e78d1345a64cf906334fd88a970b59d4a39f
gholamrezaMohammadi <gholamreza13720224@gmail.com>
2020-12-15 07:04:33 +0330

    add products

--- flask_store/admin.py
+++ flask_store/admin.py
@@ -22,7 +22,7 @@ def login():
             for user in users:
                 unciphered_pass = (cipher_suite.decrypt(bytes(user[1], 'utf-8'))).decode("utf-8")
                 if username == user[0] and password == unciphered_pass:
-                    session['username'] = username
+                    # session['username'] = username
                     return redirect(url_for("admin.product"), code=307)
             return render_template("admin_panel/login.html", error="incorect username or password")
     else:
@@ -31,4 +31,21 @@ def login():
 
 @bp.route("/product", methods=["POST"])
 def product():
-    return render_template("admin_panel/product.html", username=session['username'])
+    products = [
+        ("https://www.ibiar.com/images/6261107003705-256.jpg",
+         "لوبیا قرمز 900 گرمی گلستان",
+         "مواد غذایی / کالاهای اساسی و خوار و بار"),
+        ("https://onemarketco.ir/wp-content/uploads/8083FA0D-46D9-4EC1-B53C-3DD7E7365400.jpeg",
+         "روغن مخصوص سرخ کردنی بدون پالم 2000 میلی لیتری اویلا",
+         "مواد غذایی / کالاهای اساسی و خوار و بار"),
+        ("https://onemarketco.ir/wp-content/uploads/6EDDF87E-A388-4965-906D-3B63270AB958.jpeg",
+         "روغن مایع آفتابگردان ویتامینه 1800 میلی لیتری غنچه",
+         "مواد غذایی / کالاهای اساسی و خوار و بار"),
+        ("https://www.ibiar.com/images/6260063200845-256.jpg",
+         "کره سنتی ۱۰۰ گرمی شکلی",
+         "مواد غذایی / لبنیات"),
+        ("https://www.ibiar.com/images/8000070018686-256.jpg",
+         "پودر قهوه دم کردنی اسپرسو 250 گرمی لاواتزا",
+         "مواد غذایی / نوشیدنی")
+    ]
+    return render_template("admin_panel/product.html", products=products)
