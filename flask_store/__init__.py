b527682387062392047b46120a713f253f0700f9
Reza Ghanbari <rezaghanbarii2000@gmail.com>
2020-12-16 19:16:30 +0330

    add home url

--- flask_store/__init__.py
+++ flask_store/__init__.py
@@ -46,5 +46,6 @@ def create_app(test_config=None):
     # # app.route, while giving the blog blueprint a url_prefix, but for
     # # the tutorial the blog will be the main index
     # app.add_url_rule("/", endpoint="index")
+    app.add_url_rule("/", endpoint="home")
 
     return app
