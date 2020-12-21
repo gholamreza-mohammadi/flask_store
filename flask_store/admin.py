c28f60895b66924c2984d09eb6a4de6ec0da1ca2
gholamrezaMohammadi <gholamreza13720224@gmail.com>
2020-12-22 00:17:41 +0330

    add ajax to admin_panel -> repository, inventory and order

--- flask_store/admin.py
+++ flask_store/admin.py
@@ -38,6 +38,87 @@ def management():
         abort(404)
 
 
+@bp.route("/order-api", methods=["POST", "GET"])
+def get_orders():
+    if request.method == "POST":
+        orders = {"column_names": ["id", "user_name", "total_price", "order_time"],
+                  'data': [
+                      {"id": None,
+                       "user_name": "اکبر زمانی",
+                       "total_price": 920000,
+                       "order_time": '1399/01/05'},
+                      {"id": None,
+                       "user_name": "رامین رحیمی",
+                       "total_price": 8764300,
+                       "order_time": '1399/01/04'},
+                      {"id": None,
+                       "user_name": "ملیکا زارعی",
+                       "total_price": 3324000,
+                       "order_time": '1399/01/03'},
+                      {"id": None,
+                       "user_name": "نیکی کریمی",
+                       "total_price": 1258000,
+                       "order_time": '1399/01/02'},
+                      {"id": None,
+                       "user_name": "فریبرز عربنیا",
+                       "total_price": 540000,
+                       "order_time": '1399/01/01'}
+                  ]}
+        return orders
+    else:
+        abort(404)
+
+
+@bp.route("/inventory-api", methods=["POST", "GET"])
+def get_inventories():
+    if request.method == "POST":
+        inventories = {"column_names": ["id", "repository_name", "commodity_name", "price", "quantity"],
+                       'data': [
+                           {"id": None,
+                            "repository_name": "انبار شماره ۱",
+                            "commodity_name": "لوبیا قرمز 900 گرمی گلستان",
+                            "price": 200000,
+                            "quantity": 100},
+                           {"id": None,
+                            "repository_name": "انبار شماره ۱",
+                            "commodity_name": "روغن مخصوص سرخ کردنی بدون پالم 2000 میلی لیتری اویلا",
+                            "price": 10000,
+                            "quantity": 200},
+                           {"id": None,
+                            "repository_name": "انبار شماره ۲",
+                            "commodity_name": "روغن مایع آفتابگردان ویتامینه 1800 میلی لیتری غنچه",
+                            "price": 150000,
+                            "quantity": 300},
+                           {"id": None,
+                            "repository_name": "انبار شماره ۲",
+                            "commodity_name": "کره سنتی ۱۰۰ گرمی شکلی",
+                            "price": 25500,
+                            "quantity": 400},
+                           {"id": None,
+                            "repository_name": "انبار شماره ۳",
+                            "commodity_name": "پودر قهوه دم کردنی اسپرسو 250 گرمی لاواتزا",
+                            "price": 1000000,
+                            "quantity": 500}
+                       ]}
+        return inventories
+    else:
+        abort(404)
+
+
+@bp.route("/repository-api", methods=["POST", "GET"])
+def get_repositories():
+    if request.method == "POST":
+        repositories = {"column_names": ["id", "repository_name"],
+                        'data': [
+                            {"id": None, "repository_name": "انبار شماره ۱"},
+                            {"id": None, "repository_name": "انبار شماره ۲"},
+                            {"id": None, "repository_name": "انبار شماره ۳"}
+                        ]}
+        return repositories
+    else:
+        abort(404)
+
+
 @bp.route("/category-api", methods=["POST", "GET"])
 def get_categories():
     if request.method == "POST":
@@ -55,21 +136,26 @@ def get_categories():
 def get_products():
     if request.method == "POST":
         products = {
-            "column_names": ["link", "name", "category"],
-            "data": [{"link": "https://www.ibiar.com/images/6261107003705-256.jpg",
-                      "name": "لوبیا قرمز 900 گرمی گلستان",
+            "column_names": ["id", "image_link", "commodity_name", "category"],
+            "data": [{"id": None,
+                      "image_link": "https://www.ibiar.com/images/6261107003705-256.jpg",
+                      "commodity_name": "لوبیا قرمز 900 گرمی گلستان",
                       "category": "مواد غذایی / کالاهای اساسی و خوار و بار"},
-                     {"link": "https://onemarketco.ir/wp-content/uploads/8083FA0D-46D9-4EC1-B53C-3DD7E7365400.jpeg",
-                      "name": "روغن مخصوص سرخ کردنی بدون پالم 2000 میلی لیتری اویلا",
+                     {"id": None,
+                      "image_link": "https://onemarketco.ir/wp-content/uploads/8083FA0D-46D9-4EC1-B53C-3DD7E7365400.jpeg",
+                      "commodity_name": "روغن مخصوص سرخ کردنی بدون پالم 2000 میلی لیتری اویلا",
                       "category": "مواد غذایی / کالاهای اساسی و خوار و بار"},
-                     {"link": "https://onemarketco.ir/wp-content/uploads/6EDDF87E-A388-4965-906D-3B63270AB958.jpeg",
-                      "name": "روغن مایع آفتابگردان ویتامینه 1800 میلی لیتری غنچه",
+                     {"id": None,
+                      "image_link": "https://onemarketco.ir/wp-content/uploads/6EDDF87E-A388-4965-906D-3B63270AB958.jpeg",
+                      "commodity_name": "روغن مایع آفتابگردان ویتامینه 1800 میلی لیتری غنچه",
                       "category": "مواد غذایی / کالاهای اساسی و خوار و بار"},
-                     {"link": "https://www.ibiar.com/images/6260063200845-256.jpg",
-                      "name": "کره سنتی ۱۰۰ گرمی شکلی",
+                     {"id": None,
+                      "image_link": "https://www.ibiar.com/images/6260063200845-256.jpg",
+                      "commodity_name": "کره سنتی ۱۰۰ گرمی شکلی",
                       "category": "مواد غذایی / لبنیات"},
-                     {"link": "https://www.ibiar.com/images/8000070018686-256.jpg",
-                      "name": "پودر قهوه دم کردنی اسپرسو 250 گرمی لاواتزا",
+                     {"id": None,
+                      "image_link": "https://www.ibiar.com/images/8000070018686-256.jpg",
+                      "commodity_name": "پودر قهوه دم کردنی اسپرسو 250 گرمی لاواتزا",
                       "category": "مواد غذایی / نوشیدنی"}]
         }
         return products
--- flask_store/static/js_folder/admin_panel/base_admin.js
+++ flask_store/static/js_folder/admin_panel/base_admin.js
@@ -29,9 +29,9 @@ $(function () {
                 var counter = 0;
                 response.data.forEach(function (element) {
                     table += "<tr>";
-                    table += "<td><a href=" + element[column_names[0]] + " target='_blank'>link</a></td>";
-                    table += "<td>" + element[column_names[1]] + "</td>";
+                    table += "<td><a href=" + element[column_names[1]] + " target='_blank'>link</a></td>";
                     table += "<td>" + element[column_names[2]] + "</td>";
+                    table += "<td>" + element[column_names[3]] + "</td>";
                     table += '<td><a href="#" id="product_edit_' + counter + '">ویرایش</a> ';
                     table += '<a href="#" id="product_delete_' + counter + '">حذف</a></td>';
                     table += "</tr>";
@@ -46,11 +46,6 @@ $(function () {
                     event.preventDefault();
                     $.post("category-api", function (response, status) {
                         if (status == "success") {
-                            if ($("div#popup_div").children().length) {
-                                // for closing
-                                $("div#popup_div").empty();
-                            }
-
                             var popup = '<div class="popup">';
                             popup += "<h2>افزودن / ویرایش کالا</h2>";
                             popup += "<h4>نام کالا:</h4>";
@@ -77,7 +72,7 @@ $(function () {
                     var popup = '<div class="popup">';
                     popup += "<h2>آپلود فایل جدول کالا</h2>";
                     popup += "<h4>انتخاب فایل:</h4>";
-                    popup += '<input id="products_file" name="products_file" type="file">';
+                    popup += '<div dir="ltr"><input dir="ltr" id="products_file" name="products_file" type="file"></div>';
                     popup += '<div class="container"><a href="#" class="btn_a" id="add_products_btn">آپلود و ذخیره‌سازی جدول</a></div>';
                     popup += "</div>";
 
@@ -85,8 +80,135 @@ $(function () {
                     $("div.popup").lightbox_me();
                 });
             } else {
-                $("div#table_div").append("Error in page loading.");
+                alert("Error in received data.");
             }
         });
     });
+
+    $("a#repository").click(function (event) {
+        event.preventDefault();
+        $.post("repository-api", function (response, status) {
+            data_div_cleaner();
+            if (status == "success") {
+                var header = "<h2>مدیریت انبارها</h2>";
+                header += "<div>";
+                header += '<a href="" class="btn_a" id="add_repository_btn">افزودن انبار</a> ';
+                header += "</div>";
+
+                var column_names = response.column_names;
+                var table = '<table class="repositories_table">';
+                table += "<tr>";
+                table += "<th>" + "نام انبار" + "</th>";
+                table += "<th>" + " " + "</th>";
+                table += "</tr>";
+
+                var counter = 0;
+                response.data.forEach(function (element) {
+                    table += "<tr>";
+                    table += "<td>" + element[column_names[1]] + "</td>";
+                    table += '<td><a href="#" id="repository_edit_' + counter + '">ویرایش</a> ';
+                    table += '<a href="#" id="repository_delete_' + counter + '">حذف</a></td>';
+                    table += "</tr>";
+                    counter++;
+                });
+                table += "</table>";
+
+                $("div#header_div").append(header);
+                $("div#table_div").append(table);
+                
+                $("a#add_repository_btn").click(function (event) {
+                    event.preventDefault();
+                    console.log("add_repository_btn");
+                });
+                
+            } else {
+                alert("Error in received data.");
+            }
+        });
+    });
+
+    $("a#inventory").click(function (event) {
+        event.preventDefault();
+        $.post("inventory-api", function (response, status) {
+            data_div_cleaner();
+            if (status == "success") {
+                var header = "<h2>مدیریت موجودی و قیمت‌ها</h2>";
+                header += "<div>";
+                header += '<a href="" class="btn_a" id="add_inventory_btn">افزودن موجودی</a> ';
+                header += "</div>";
+
+                var column_names = response.column_names;
+                var table = '<table>';
+                table += "<tr>";
+                table += "<th>" + "انبار" + "</th>";
+                table += "<th>" + "کالا" + "</th>";
+                table += "<th>" + "قیمت" + "</th>";
+                table += "<th>" + "موجودی" + "</th>";
+                table += "<th>" + " " + "</th>";
+                table += "</tr>";
+
+                var counter = 0;
+                response.data.forEach(function (element) {
+                    table += "<tr>";
+                    table += "<td>" + element[column_names[1]] + "</td>";
+                    table += "<td>" + element[column_names[2]] + "</td>";
+                    table += "<td>" + element[column_names[3]] + "</td>";
+                    table += "<td>" + element[column_names[4]] + "</td>";
+                    table += '<td><a href="#" id="inventory_edit_' + counter + '">ویرایش</a> ';
+                    table += '<a href="#" id="inventory_delete_' + counter + '">حذف</a></td>';
+                    table += "</tr>";
+                    counter++;
+                });
+                table += "</table>";
+
+                $("div#header_div").append(header);
+                $("div#table_div").append(table);
+                
+                $("a#add_inventory_btn").click(function (event) {
+                    event.preventDefault();
+                    console.log("add_inventory_btn");
+                });
+                
+            } else {
+                alert("Error in received data.");
+            }
+        });
+    });
+
+    $("a#order").click(function (event) {
+        event.preventDefault();
+        $.post("order-api", function (response, status) {
+            data_div_cleaner();
+            if (status == "success") {
+                var header = "<h2>مدیریت سفارش‌ها</h2>";
+
+                var column_names = response.column_names;
+                var table = '<table>';
+                table += "<tr>";
+                table += "<th>" + "نام کاربر" + "</th>";
+                table += "<th>" + "مجموع مبلغ" + "</th>";
+                table += "<th>" + "زمان ثبت سفارش" + "</th>";
+                table += "<th>" + " " + "</th>";
+                table += "</tr>";
+
+                var counter = 0;
+                response.data.forEach(function (element) {
+                    table += "<tr>";
+                    table += "<td>" + element[column_names[1]] + "</td>";
+                    table += "<td>" + element[column_names[2]] + "</td>";
+                    table += "<td>" + element[column_names[3]] + "</td>";
+                    table += '<td><a href="#" id="order_detail_' + counter + '">بررسی سفارش</a></td>';
+                    table += "</tr>";
+                    counter++;
+                });
+                table += "</table>";
+
+                $("div#header_div").append(header);
+                $("div#table_div").append(table);
+            } else {
+                alert("Error in received data.");
+            }
+        });
+    });
+
 });
