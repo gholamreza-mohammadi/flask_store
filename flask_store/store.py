98d2c8dcde14cea42624d1b1993a4111e58a15c0
gholamrezaMohammadi <gholamreza13720224@gmail.com>
2020-12-23 16:13:32 +0330

    -_-

--- flask_store/store.py
+++ flask_store/store.py
@@ -6,10 +6,11 @@ from flask import current_app
 from flask import url_for
 from flask import redirect
 from flask import session
+from flask import abort
 
 bp = Blueprint("store", __name__)
 
 
-@bp.route("/", methods=("GET", "POST"))
+@bp.route("/", methods=["GET", "POST"])
 def home():
     return render_template("store/base_store.html")
