7ebd7f5e68b9338514736daf4f59b9b74ab4e583
Reza Ghanbari <rezaghanbarii2000@gmail.com>
2020-12-27 15:21:53 +0330

    Optimization in db file

--- flask_store/db.py
+++ flask_store/db.py
@@ -3,17 +3,16 @@ from bson import ObjectId
 from datetime import datetime
 
 client = MongoClient('localhost', 27017)
+db = client.store
 
 #=============================products=============================
 def get_products():
-    db = client.store
     products = list(db.products.find())
     for p in products:
         p['_id'] = str(p['_id'])
     return products
 
 def add_product(data):
-    db = client.store
     if not (list(db.products.find({"commodity_name":data['product_name']}))):
         db.products.insert_one({
         "image_link": "",
@@ -22,11 +21,9 @@ def add_product(data):
         })
 
 def delete_product(data):
-    db = client.store
     db.products.delete_one({"_id": ObjectId(data['product_id'])})
     
 def edit_product(data):
-    db = client.store
     db.products.update_one(
         {"_id":ObjectId(data['product_id'])},
         {'$set':{
@@ -38,23 +35,19 @@ def edit_product(data):
 
 #=============================repositories=============================
 def get_repositories():
-    db = client.store
     repositories = list(db.repositories.find())
     for p in repositories:
         p['_id'] = str(p['_id'])
     return repositories
 
 def add_repositories(data):
-    db = client.store
     if not (list(db.repositories.find({"repository_name":data['repository_name']}))):
         db.repositories.insert_one({
         "repository_name": data['repository_name']
         })
 
 def delete_repositories(data):
-    db = client.store
     db.repositories.delete_one({"_id": ObjectId(data['repository_id'])})
    
 def edit_repository(data):
-    db = client.store
     db.repositories.update_one({"_id":ObjectId(data['repository_id'])},{'$set':{'repository_name':data['repository_name']}})
