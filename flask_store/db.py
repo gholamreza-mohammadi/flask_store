751aba001311f69cababed242c4920f982b7583a
Reza Ghanbari <rezaghanbarii2000@gmail.com>
2020-12-30 23:19:54 +0330

    modify database and update inventory with update product

--- flask_store/db.py
+++ flask_store/db.py
@@ -17,8 +17,11 @@ def get_products():
 
 def add_product(data):
     if not (list(db.products.find({"commodity_name": data['product_name']}))):
+        img_link = data['product_image_link']
+        if not img_link:
+            img_link = "https://appiranie.websites.co.in/e-store/img/defaults/product-default.png"
         db.products.insert_one({
-            "image_link": data['product_image_link'],
+            "image_link": img_link,
             "commodity_name": data['product_name'],
             "category": data['product_category'],
             "description": data['product_description']
@@ -30,15 +33,20 @@ def delete_product(data):
 
 
 def edit_product(data):
+    img_link = data['product_image_link']
+    if not img_link:
+        img_link = "https://appiranie.websites.co.in/e-store/img/defaults/product-default.png"
+    last_name = list(db.products.find({"_id": ObjectId(data['product_id'])},{'commodity_name':1,'_id':0}))[0]['commodity_name']
     db.products.update_one(
         {"_id": ObjectId(data['product_id'])},
         {'$set': {
-            "image_link": data['product_image_link'],
+            "image_link": img_link,
             'commodity_name': data['product_name'],
             'category': data['product_category'],
             "description": data['product_description']
         }
         })
+    update_inventory(data,img_link,last_name)
 
 
 # =============================repositories=============================
@@ -74,10 +82,10 @@ def get_inventories():
     return inventories
 
 
-def get_category(product):
+def get_attr_product(product):
     return list(db.products.find(
         {"commodity_name": product},
-        {'_id': 0, 'category': 1}))[0]['category']
+        {'_id': 0, 'category': 1, 'image_link': 1,'description':1}))[0]
 
 
 def add_inventory(data):
@@ -85,12 +93,15 @@ def add_inventory(data):
         'commodity_name': data['inventory_product'],
         'repository_name': data['inventory_repository']})
     if not list(exist):
+        product = data['inventory_product']
         db.inventories.insert_one({
-            "commodity_name": data['inventory_product'],
-            "category": get_category(data['inventory_product']),
+            "commodity_name": product,
+            "category": get_attr_product(product)["category"],
             "price": data['inventory_price'],
             "quantity": data['inventory_quantity'],
             "repository_name": data['inventory_repository'],
+            "image_link": get_attr_product(product)["image_link"],
+            "description": get_attr_product(product)["description"],
             "create_time": datetime.utcnow()
         })
 
@@ -100,17 +111,29 @@ def delete_inventory(data):
 
 
 def edit_inventory(data):
+    product = data['inventory_product']
     db.inventories.update_one(
         {"_id": ObjectId(data['inventory_id'])},
         {'$set': {
-            "commodity_name": data['inventory_product'],
-            "category": get_category(data['inventory_product']),
+            "commodity_name": product,
+            "category": get_attr_product(product)["category"],
             "price": data['inventory_price'],
             "quantity": data['inventory_quantity'],
             "repository_name": data['inventory_repository'],
+            "image_link": get_attr_product(product)["image_link"],
             "create_time": datetime.utcnow()
         }})
 
+def update_inventory(data,img_link,last_name):
+    id = list(db.inventories.find({'commodity_name':last_name}))[0]['_id']
+    db.inventories.update_one(
+        {"_id": ObjectId(id)},
+        {'$set': {
+            "image_link":img_link,
+            "commodity_name": data['product_name'],
+            "category": data['product_category'],
+            "description": data["product_description"]
+        }})
 
 # =============================repositories=============================
 def get_orders():
